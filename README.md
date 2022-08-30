2c1e6d355a2669d3038742c52e1048df6a30f3bc
Anshu Krishna <anshu.krishna5@gmail.com>
2022-08-30 11:41:45 +0530

    sync

--- README.md
+++ README.md
@@ -2,14 +2,63 @@
 
 PHP driver for Bolt protocol for communication with Neo4j graph database over TCP
 
-## Installation
+## Installation:
 ```
 composer require anshu-krishna/neo4j-bolt
 ```
 
-## Requirements
+## Requirements:
 - PHP >= 8.1
 - Neo4j (with Bolt version 4.4, 4.3, 4.2 or 4.1)
 
 **Note: All examples use the Neo4j 'Example Movie Database'**
 
+## Example:
+```php
+<?php
+require_once "vendor/autoload.php";
+
+use Krishna\Neo4j\{AuthToken, BoltMaker};
+use Krishna\Neo4j\Protocol\Reply\Success;
+
+// Create Bolt
+$bolt = (new BoltMaker(auth: AuthToken::basic('neo4j', 'open')))->makeBolt();
+
+echo 'Running Bolt version:', $bolt::VERSION, '<br>';
+
+// Run Query
+$run = $bolt->query(<<<CYPHER
+match (p:Person)-[a:ACTED_IN]->(m:Movie)
+return
+	p as person,
+	a.roles as roles,
+	{title: m.title, year: m.released} as movie
+limit 2
+CYPHER);
+
+if($run instanceof Success) {
+	echo <<<HTML
+<table border="1">
+	<tr>
+		<th>Person</th> <th>Born</th> <th>Role(s)</th> <th>Movie</th> <th>Released</th>
+	</tr>
+HTML;
+	// Pull results
+	foreach($bolt->pull() as $i) {
+		$roles = implode(', ', $i->roles);
+		echo <<<"ROW"
+<tr>
+	<td>{$i->person->properties['name']}</td>
+	<td>{$i->person->properties['born']}</td>
+	<td>{$roles}</td>
+	<td>{$i->movie['title']}</td>
+	<td>{$i->movie['year']}</td>
+</tr>
+ROW;
+	}
+
+	echo '</table>';
+} else {
+	echo 'Query Error: ', $run->message;
+}
+```
--- example.php
+++ example.php
@@ -1,30 +1,46 @@
 <?php
-
 require_once "vendor/autoload.php";
 
-use Krishna\Neo4j\{AuthToken, BoltMaker, Logger, E_Version as Ver};
-use Krishna\Neo4j\Conn\E_ConnType;
+use Krishna\Neo4j\{AuthToken, BoltMaker};
+use Krishna\Neo4j\Protocol\Reply\Success;
 
-set_time_limit(5);
+// Create Bolt
+$bolt = (new BoltMaker(auth: AuthToken::basic('neo4j', 'open')))->makeBolt();
 
-$neo = new BoltMaker(
-	auth: AuthToken::basic('neo4j', 'open'),
-	// logger: new Logger(rowSize: 40)
-);
-// $bolt = $neo->useVersion(Ver::V4_1)->makeBolt();
-$bolt = $neo->makeBolt();
-var_dump($bolt::VERSION);
+echo 'Running Bolt version:', $bolt::VERSION, '<br>';
 
-$bolt->beginTransaction();
-// $bolt->commit();
+// Run Query
+$run = $bolt->query(<<<CYPHER
+match (p:Person)-[a:ACTED_IN]->(m:Movie)
+return
+	p as person,
+	a.roles as roles,
+	{title: m.title, year: m.released} as movie
+limit 2
+CYPHER);
 
+if($run instanceof Success) {
+	echo <<<HTML
+<table border="1">
+	<tr>
+		<th>Person</th> <th>Born</th> <th>Role(s)</th> <th>Movie</th> <th>Released</th>
+	</tr>
+HTML;
+	// Pull results
+	foreach($bolt->pull() as $i) {
+		$roles = implode(', ', $i->roles);
+		echo <<<"ROW"
+<tr>
+	<td>{$i->person->properties['name']}</td>
+	<td>{$i->person->properties['born']}</td>
+	<td>{$roles}</td>
+	<td>{$i->movie['title']}</td>
+	<td>{$i->movie['year']}</td>
+</tr>
+ROW;
+	}
 
-// $bolt->logger = new Logger(rowSize: 40);
-
-$bolt->query('match (p:Person)-[a:ACTED_IN]->(m:Movie)<-[d:DIRECTED]-(p) return p.name as person, a.roles as role, collect(m.title) as movie limit 5');
-
-var_dump($bolt->pull(2));
-// var_dump($bolt->discard(1));
-var_dump($bolt->pull());
-var_dump($bolt->getQueryMeta());
-$bolt->rollback();
+	echo '</table>';
+} else {
+	echo 'Query Error: ', $run->message;
+}
