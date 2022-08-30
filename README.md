a813f656975e8fd4510936431d46c982c4d0628a
Anshu Krishna <anshu.krishna5@gmail.com>
2022-08-30 11:06:35 +0530

    adding readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Krishna-Neo4j-Bolt
+
+PHP driver for Bolt protocol for communication with Neo4j graph database over TCP
+
+## Installation
+```
+composer require anshu-krishna/neo4j-bolt
+```
+
+## Requirements
+- PHP >= 8.1
+- Neo4j (with Bolt version 4.4, 4.3, 4.2 or 4.1)
+
+**Note: All examples use the Neo4j 'Example Movie Database'**
+
--- example.php
+++ example.php
@@ -9,19 +9,22 @@ set_time_limit(5);
 
 $neo = new BoltMaker(
 	auth: AuthToken::basic('neo4j', 'open'),
-	logger: new Logger(rowSize: 40)
+	// logger: new Logger(rowSize: 40)
 );
 // $bolt = $neo->useVersion(Ver::V4_1)->makeBolt();
 $bolt = $neo->makeBolt();
 var_dump($bolt::VERSION);
 
-// $bolt->beginTransaction();
+$bolt->beginTransaction();
 // $bolt->commit();
-// $bolt->rollback();
+
 
 // $bolt->logger = new Logger(rowSize: 40);
 
-var_dump($bolt->query('match (p:Person)-[a:ACTED_IN]->(m:Movie)<-[d:DIRECTED]-(p) return p.name as person, a.roles as role, collect(m.title) as movie limit 2'));
+$bolt->query('match (p:Person)-[a:ACTED_IN]->(m:Movie)<-[d:DIRECTED]-(p) return p.name as person, a.roles as role, collect(m.title) as movie limit 5');
 
 var_dump($bolt->pull(2));
+// var_dump($bolt->discard(1));
+var_dump($bolt->pull());
 var_dump($bolt->getQueryMeta());
+$bolt->rollback();
